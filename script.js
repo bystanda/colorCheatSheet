cf79bc0a1c137f56b10e590e9e742259c34f14d4
bystand <92830997+bystanda@users.noreply.github.com>
2021-12-20 21:32:25 +0000

    Added accessibility button

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function loadImage() {
   var randomColor = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor
 }
+
+function fontChange() {
+  document.getElementById('header').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('hue').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('hueEx').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('type').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('typeEx').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('var').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('varEx').style.fontFamily = "'Rambla', sans-serif"
+  document.getElementById('links').style.fontFamily = "'Rambla', sans-serif"
+  console.log('Works?')
+}
